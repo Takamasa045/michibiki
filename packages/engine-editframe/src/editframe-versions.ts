fb1558c1ed8ad712ee55fec4f1b00bb59c7d6f8e
Itopan <tkms045@gmail.com>
2026-06-13 14:29:39 +0000

    fix(deps): update video engines

--- packages/engine-editframe/src/editframe-versions.ts
+++ packages/engine-editframe/src/editframe-versions.ts
@@ -1,13 +1,13 @@
 // renovate: datasource=npm depName=@editframe/api versioning=npm
-const EDITFRAME_API_VERSION = "^0.54.0";
+const EDITFRAME_API_VERSION = "^0.57.0";
 // renovate: datasource=npm depName=@editframe/cli versioning=npm
-const EDITFRAME_CLI_VERSION = "^0.54.0";
+const EDITFRAME_CLI_VERSION = "^0.57.0";
 // renovate: datasource=npm depName=@editframe/create versioning=npm
-const EDITFRAME_CREATE_VERSION = "^0.54.0";
+const EDITFRAME_CREATE_VERSION = "^0.57.0";
 // renovate: datasource=npm depName=@editframe/elements versioning=npm
-const EDITFRAME_ELEMENTS_VERSION = "^0.54.0";
+const EDITFRAME_ELEMENTS_VERSION = "^0.57.0";
 // renovate: datasource=npm depName=@editframe/react versioning=npm
-const EDITFRAME_REACT_VERSION = "^0.54.0";
+const EDITFRAME_REACT_VERSION = "^0.57.0";
 
 export const EDITFRAME_PACKAGE_VERSIONS = {
   "@editframe/api": EDITFRAME_API_VERSION,
